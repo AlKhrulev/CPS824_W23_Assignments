7c8effdeae8a01f059b3d1e3bcfd74d8a668da5e
AlKhrulev <51909173+AlKhrulev@users.noreply.github.com>
2023-01-27 15:29:44 -0500

    Updated Question 2
    1) Replaced the optimal_action calculation
    2) Changed the way Q_t is updated
    3) Introduced a new way to generate the last_action

--- question_2.py
+++ question_2.py
@@ -1,21 +1,71 @@
 import numpy as np
 
 
+def find_position(p_t, random_value=None):
+    # TODO add more documentation
+    # we generate/use a random number in the range from 0 to 1
+    # sort the array
+    # find the index where the random number would need to be inserted
+    # to maintain the order. The prob. at that index is the one closest to ours.
+    # Now, find the index of that closest probability in the original array.
+    # That number will serve as a random action we want to take
+    """
+    Sample Usage:
+    random_value=0.6341171045877046
+p_t=array([0.19399856, 0.53831702, 0.00435142, 0.71462101, 0.57039106,
+       0.79182353, 0.98224042, 0.78473985, 0.89165213, 0.69857165])
+p_t_sorted=array([0.00435142, 0.19399856, 0.53831702, 0.57039106, 0.69857165,
+       0.71462101, 0.78473985, 0.79182353, 0.89165213, 0.98224042])
+position_in_sorted=4
+closest_probability=0.6985716477516771
+true_position=array([9], dtype=int64)
+random_value=0.7159561528061424
+p_t=array([0.19399856, 0.53831702, 0.00435142, 0.71462101, 0.57039106,
+       0.79182353, 0.98224042, 0.78473985, 0.89165213, 0.69857165])
+p_t_sorted=array([0.00435142, 0.19399856, 0.53831702, 0.57039106, 0.69857165,
+       0.71462101, 0.78473985, 0.79182353, 0.89165213, 0.98224042])
+position_in_sorted=6
+closest_probability=0.7847398506606472
+true_position=array([7], dtype=int64)
+random_value=0.12892700807072133
+p_t=array([0.19399856, 0.53831702, 0.00435142, 0.71462101, 0.57039106,
+       0.79182353, 0.98224042, 0.78473985, 0.89165213, 0.69857165])
+p_t_sorted=array([0.00435142, 0.19399856, 0.53831702, 0.57039106, 0.69857165,
+       0.71462101, 0.78473985, 0.79182353, 0.89165213, 0.98224042])
+position_in_sorted=1
+closest_probability=0.1939985555898357
+true_position=array([0], dtype=int64)
+    """
+    if not random_value:
+        random_value = np.random.rand()
+    p_t_sorted = np.sort(p_t)
+    position_in_sorted = np.searchsorted(p_t_sorted, random_value)
+    print(f'{position_in_sorted=}')
+    closest_probability = p_t_sorted[position_in_sorted]
+    true_position = np.where(np.isclose(p_t, closest_probability))[0]
+    print(
+        f"{random_value=}\n{p_t=}\n{p_t_sorted=}\n{position_in_sorted=}\n{closest_probability=}\n{true_position=}"
+    )
+    return true_position
+
 # CONSTANTS
-EPOCH_NUM = 5000
+EPOCH_NUM = 30000
 ALPHA = 0.1
 BETA = 0
 
 
-# up to 65535 max
-Q_t = np.zeros(10, dtype=np.uint16)  # accumulated rewards for each action
-N_t = np.zeros(10, dtype=np.uint16)  # number of times each action was taken
+Q_t = np.zeros(10, dtype=np.float64)  # accumulated rewards for each action
+N_t = np.zeros(
+    10, dtype=np.uint16
+)  # number of times each action was taken(up to 65535 max)
 # probabilities=np.random.dirichlet(np.ones(10),size=1).reshape(10,)
 
 # an array of actual probabilities~continuous U(0,1)
 probabilities = np.random.rand(10)
 # cache response based on the probabilities(for fast selection)
 response = (probabilities >= 0.5).astype(np.uint8)
+# the index of action that has the highest prob. of success
+optimal_action = np.argmax(probabilities)
 
 
 # TODO del
@@ -31,18 +81,19 @@ print(p_t, probabilities, response)
 mask = np.zeros_like(p_t, dtype=bool)
 
 for epoch in range(EPOCH_NUM):
-    last_action = np.random.randint(0, 10, dtype=np.uint8)
+    
+    # last_action = np.random.randint(0, 10, dtype=np.uint8)
+    last_action= find_position(probabilities,None)
     last_signal = response[last_action]
-    optimal_action = np.argmax(Q_t)
 
     # modify total optimal number if the last action was indeed optimal
-    total_optimal_num += (optimal_action == last_action)
+    total_optimal_num += optimal_action == last_action
 
     # update the action counter
     N_t[last_action] += 1
     # update the accumulated reward for that action
     # TODO check if this update is correct
-    Q_t[last_action] += last_signal
+    Q_t[last_action] += (1 / (epoch + 1)) * (last_action - Q_t[last_action])
 
     # set the last action to 1 for future selection
     mask[last_action] = 1
@@ -59,10 +110,16 @@ for epoch in range(EPOCH_NUM):
     # reset the mask
     mask[:] = 0
 
-    if epoch % 100 == 0:
+    # if epoch % 100 == 0:
+    if epoch == (EPOCH_NUM - 1):
         print(f"FOR {epoch=}:\n-----------------")
         print(f"the optimal action was taken {total_optimal_num} times")
         print(f"the average reward is {Q_t}\n\n")
 
 # TODO delete(used only for debugging)
 print(p_t, N_t, Q_t, sep="\n")
+
+
+
+# for i in range(3):
+#     find_position(probabilities,None)
