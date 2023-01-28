216a827ba3ed5d071a6a62bf046031b30ef973a0
Mahan Pandey <superMsp10@users.noreply.github.com>
2023-01-28 17:11:33 -0500

    Q1 report

--- q1.py
+++ q1.py
@@ -1,42 +1,19 @@
-import numpy
+import numpy as np
 from numpy import random
 
-
-numOptimalActionChosen = 0
-totalReward = 0
-
-q = random.rand(10)
-trueOptimalAction = 0
-maxValue = -float("inf")
-for i in range(10):
-    if(q[i] > maxValue):
-        maxValue = q[i]
-        trueOptimalAction = i
-        
-Q = [0] * 10
-N = [0] * 10
+results = np.zeros(shape=(100,50))
 # Exploration constant
-c = 1
+c = 0.01
 
-def TryAction(action):
-    global numOptimalActionChosen, totalReward
-    N[action] += 1
-    if(action == trueOptimalAction):
-        numOptimalActionChosen += 1
-    v = random.rand()
-    # print("TryAction", v, q[action])
-    Rt = v < q[action]
-    totalReward += Rt
-    return Rt
 
-def FindMaxAction(t):
+def FindMaxAction(Q, t):
     maxValue = -float("inf")
     maxIndex = 0
     for i in range(10):
         Ucb = 0
         # Upper confidence bound
         if N[i] != 0:
-            Ucb = Q[i] + c * numpy.sqrt(numpy.log(t) / N[i])
+            Ucb = Q[i] + c * np.sqrt(np.log(t) / N[i])
         else:
             # If Nt(a) = 0, then a is considered to be a maximizing action
             Ucb = float("inf")
@@ -45,16 +22,36 @@ def FindMaxAction(t):
             maxIndex = i
     return maxIndex
 
-# Interactions with the environment
-for i in range(0, 5000):
-    # find action that maximizes UCB
-    At = FindMaxAction(i)
-    # Try action, record reward from environment
-    Rt = TryAction(At)
-    Q[At] = Q[At] + 1/N[At] * (Rt - Q[At])
-    # print(At, Rt, Q)
-    if(i % 100 == 0):
-        print(i, "optimal action chosen: ", numOptimalActionChosen, ", average reward over time: ", totalReward / i)
+for epoch in range(0,100):
+    numOptimalActionChosen = 0
+    totalReward = 0
+
+    q = random.rand(10)
+    trueOptimalAction = np.argmax(q)
+            
+    Q = np.zeros(10)
+    N = np.zeros(10)
+    
+    # Interactions with the environment
+    for i in range(1, 5001):
+        # find action that maximizes UCB
+        At = FindMaxAction(Q, i)
+        
+        # Try action, record reward from environment
+        N[At] += 1
+        if(At == trueOptimalAction):
+            numOptimalActionChosen += 1
+        v = random.rand()
+        Rt = v < q[At]
+        totalReward += Rt
+
+        # Integrate reward into Q
+        Q[At] = Q[At] + 1/N[At] * (Rt - Q[At])
+        # print(At, Rt, Q)
+        if(i % 100 == 0):
+            print(epoch, i, "optimal action: ", numOptimalActionChosen, ", average reward: ", totalReward / i)
+            results[epoch, i // 100 - 1] = totalReward / i
 
 # print("true value", q, "\nagent estimate", Q)
-# print(numOptimalActionChosen)
+# print(results[0],results[50],results[99])
+np.savetxt("results.csv", results, delimiter=",")
